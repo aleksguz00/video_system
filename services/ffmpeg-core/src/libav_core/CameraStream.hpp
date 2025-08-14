38fef741cfc7f4169f7989c5e04b4911854ddc76
Alex Guz <alexguz8279@gmail.com>
2025-08-14 09:52:19 +0500

    Config fix

---
+++ services/ffmpeg-core/src/libav_core/CameraStream.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Poco/Runnable.h>
+
+class CameraStream : public Poco::Runnable
+{
+public:
+    
+
+private:
+
+};
--- services/ffmpeg-core/src/models/CameraModel.hpp
+++ services/ffmpeg-core/src/models/CameraModel.hpp
@@ -5,6 +5,8 @@ struct CameraModel
     std::string name;
     std::string address;
     std::string rtsp;
+    std::string login;
+    std::string password;
     int id;
     bool bActive;
     bool bRecordArchive;
